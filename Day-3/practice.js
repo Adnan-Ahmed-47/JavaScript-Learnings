5f4c918c82142bcaba5cd3edbc1594f0b3070315
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2026-06-18 00:25:34 +0530

    Practice questions done

---
+++ Day-3/practice.js
@@ -0,0 +1,68 @@
+var x = 20;
+x = 10;
+
+
+// let x = 10;
+// x = 1000;
+
+
+// console.log(x);
+
+
+const PI = 3.14;
+
+// PI = 4;
+
+// console.log(PI);
+
+// console.log(Boolean())
+
+
+let name = "Adnan";
+const country = "India";
+
+// console.log(typeof true);
+// console.log(typeof 100);
+
+// console.log(typeof Number("50"));
+// console.log(typeof String(500));
+
+
+let num1 = 1;
+let num2 = 0;
+// console.log(Boolean(num1));
+// console.log(Boolean(num2));
+// console.log(typeof Boolean(1));
+
+
+let res; 
+// console.log(res);
+
+let val = null;
+
+// console.log(val);
+// console.log(typeof val);
+
+// console.log(Boolean(" "));
+// console.log(Number(true));
+// console.log(String(true));
+
+// console.log(typeof NaN)
+
+// console.log(Boolean("false"));
+// console.log(Boolean("0"));
+// console.log(Number(""));
+// console.log(Number(" "));
+// console.log(Number(null));
+// console.log(Number(undefined));
+// console.log(typeof NaN);
+// console.log(typeof Symbol("id"));
+// console.log(typeof NaN);
+
+let value = "25";
+
+let num = Number(value);
+
+let result = Boolean(num);
+
+console.log(result);

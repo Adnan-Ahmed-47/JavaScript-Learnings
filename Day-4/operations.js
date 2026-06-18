4f675cee3b2e43a2505e7959b92dddc23b2502aa
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2026-06-18 23:40:12 +0530

    operations and comparisons done

---
+++ Day-4/operations.js
@@ -0,0 +1,61 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello";
+let str2 = " adnan";
+
+let str3 = str1 + str2; 
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
+
+// console.table([num1, num2, num3]);
+
+
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+// ----------------------------------------------------
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+

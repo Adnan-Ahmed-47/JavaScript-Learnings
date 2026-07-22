497dbd7518598da2be846dff18de28adeae3635d
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2026-07-22 22:19:56 +0530

    Questions Solved

--- Day-19/practice.js
+++ Day-19/practice.js
@@ -131,20 +131,249 @@ course.forEach((item) => {
 // console.log(newArr)
 
 
-const students = [
-    {name: "Adnan", marks: 97},
-    {name: "Noman", marks: 61},
-    {name: "Faizan", marks: 45},
-    {name: "Mannan", marks: 99},
-    {name: "Ismail", marks: 80},
-    {name: "Muneeb", marks: 81},
-    {name: "Viqar", marks: 68},
-    {name: "Sameer", marks: 53},
-    {name: "Mudabbir", marks: 100}
-]
+// const students = [
+//     {name: "Adnan", marks: 97},
+//     {name: "Noman", marks: 61},
+//     {name: "Faizan", marks: 45},
+//     {name: "Mannan", marks: 99},
+//     {name: "Ismail", marks: 80},
+//     {name: "Muneeb", marks: 81},
+//     {name: "Viqar", marks: 68},
+//     {name: "Sameer", marks: 53},
+//     {name: "Mudabbir", marks: 99}
+// ]
+// const topperStudents = students.filter((item) => (
+//     item.marks > 80
+// ))
+// console.log(topperStudents)
 
-const topperStudents = students.filter((item) => (
-    item.marks > 80
-))
-console.log(topperStudents)
+// const totalMarks = students.reduce((acc, curr) => (
+//     acc + curr.marks
+// ), 0)
+// console.log(`Total Marks: ${totalMarks}`)
+
+
+// const obj = {
+//     name: "Adnan",
+//     city: "Hyderabad"
+// }
+// for (const key in obj) {
+//     console.log(key)
+// }
+
+
+// const books = [
+//     {title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004},
+//     {title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008},
+//     {title: 'Book Three', genre: 'History', publish: 1999, edition: 2007},
+//     {title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010},
+//     {title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014},
+//     {title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010},
+//     {title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996},
+//     {title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016},
+//     {title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989}
+// ] 
+// const userBooks = books.filter((book) => book.publish > 2000)
+// console.log(userBooks)
+
+
+// for(let i=1; i<=100; i++) {
+//     if(i%2 == 1) {
+//         console.log(i)
+//     }
+// }
+
+
+// const shoppingCart = [
+//     {
+//         itemName: "js course",
+//         price: 2999
+//     },
+//     {
+//         itemName: "py course",
+//         price: 999
+//     },
+//     {
+//         itemName: "mobile dev course",
+//         price: 5999
+//     },
+//     {
+//         itemName: "data science course",
+//         price: 12999
+//     },
+// ]
+// const totalPrice = shoppingCart.reduce((acc, curr) => acc + curr.price, 0)
+// console.log(totalPrice)
+
+
+// function writeTable(arr) {
+//     arr.forEach((item) => {
+//         for(let i=1; i<=10; i++) {
+//             console.log(`${item} * ${i} = ${item * i}`)
+//         }
+//     })
+// }
+// writeTable([2, 4, 6])
+// function listItems(items) {
+//   items.forEach((item, index) => {
+//     console.log(`${index + 1}. ${item}`);
+//   });
+// }
+// listItems(["Apple", "Banana", "Cherry"]);
+
+
+// let i=0
+// while (i<=100) {
+//     if(i%2 == 0) {
+//         console.log(i)
+//     }
+//     i++
+// }
+
+
+// const arr = [1, 2, 3]
+// const newArr = arr.map((item) => item * item)
+// console.log(newArr)
+
+
+// const arr = [1, 2, 3, 4, 5]
+// const newArr = arr.forEach((item) => {
+//     console.log(item)
+//     return item // undefined
+// })
+// console.log(newArr) // returns undefined, bcoz forEach won't return any value, it just prints it.
+// const newArr = arr.map((item) => {
+//     console.log(item)
+//     return item [1, 2, 3, 4, 5]
+// })
+// console.log(newArr) // return new Array, it prints as well as returns the value. 
+
+
+
+// +++++++++++++++++++++++++++++++++++++++ HARD QUESTIONS +++++++++++++++++++++++++++++++++++++++++++++++
+
+// const students = [
+//     {name: "A", marks: 90},
+//     {name: "B", marks: 70},
+//     {name: "C", marks: 95}
+// ]
+// AI solution
+// function findTopper(studentList) {
+//     let topper = studentList[0]
+//     studentList.forEach((student) => {
+//         if(student.marks > topper.marks) {
+//             topper = student
+//         }
+//     })
+//     return topper
+// }
+// console.log(findTopper(students))
+
+// My way
+// let topMarks = students[0]
+// const topperStudent = students.map((student) => {
+//     if(student.marks > topMarks.marks) {
+//         topMarks = student
+//     }
+//     return topMarks
+// })
+// console.log(topMarks)
+
+
+
+
+// const students = [
+//     {name: "A", marks: 90},
+//     {name: "B", marks: 30},
+//     {name: "C", marks: 95},
+//     {name: "D", marks: 22}
+// ]
+// const failedStudents = students.filter((student) => student.marks < 35)
+// console.log(failedStudents)
+
+
+// function calculateAverage(studentsList) {
+//     const totalMarks = students.reduce((acc, student) => {
+//         return acc + student.marks
+//     }, 0)
+
+//     return totalMarks/studentsList.length;
+
+// }
+// console.log(calculateAverage(students))
+
+
+// const students = [
+//     {name: "A", marks: 90, city: "Hyderabad"},
+//     {name: "B", marks: 30, city: "Banglore"},
+//     {name: "C", marks: 95, city: "Agra"},
+//     {name: "D", marks: 22, city: "Chennai"}
+// ]
+// const hydStudents = students.filter((student) => student.city === "Hyderabad")
+// console.log(hydStudents)
+
+
+// const employees = [
+//   { name: "Alice", salary: 5000 },
+//   { name: "Bob", salary: 6000 },
+//   { name: "Charlie", salary: 4500 }
+// ];
+// const totalSalary = employees.reduce((acc, employee) => {
+//     return acc + employee.salary
+// }, 0)
+// console.log(totalSalary)
+
+
+// const books = [
+//     {title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004},
+//     {title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008},
+//     {title: 'Book Three', genre: 'History', publish: 1999, edition: 2007},
+//     {title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010},
+//     {title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014},
+//     {title: 'Book Six', genre: 'Science', publish: 1987, edition: 2010},
+//     {title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996},
+//     {title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016},
+//     {title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989}
+// ] 
+// function countBooks(booksList) {
+//     let count = 0;
+
+//     booksList.forEach((book) => {
+//         if(book.genre === "Science") {
+//             count++
+//         }
+//     })
+//     return count;
+// }
+// console.log(countBooks(books))
+
+
+
+// for(let i=1; i<=5; i++) {
+//     let rowStr = ""
+//     for(let j=1; j<=i; j++) {
+//         rowStr += "*"
+//     }
+//     console.log(rowStr)
+// }
+// OR
+// function printPattern(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     // repeat() creates a string with 'i' number of stars
+//     console.log("*".repeat(i));
+//   }
+// }
+// printPattern(5);
+
+
+
+for(let i=1; i<=5; i++) {
+    let rowStr = "";
+    for(j=1; j<=i; j++) {
+        rowStr += j
+    }
+    console.log(rowStr)
+}
+
+
 

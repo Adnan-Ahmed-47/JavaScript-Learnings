7e5e0c0e7a7323ef5de0d641b81f88bff4a0a37b
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2026-08-30 20:07:37 +0530

    Practice done

--- Day-26/script.js
+++ Day-26/script.js
@@ -1,11 +1,24 @@
-let num = 33;
-num = 35;
-console.log(num)
+// let num = 33;
+// num = 35;
+// console.log(num)
 
-var num1 = 100;
-num1 = 102;
-console.log(num1)
+// var num1 = 100;
+// num1 = 102;
+// console.log(num1)
 
-const num2 = 11;
-num2 =12;
-console.log(num2)
+// const num2 = 11;
+// num2 =12;
+// console.log(num2)
+
+
+// console.log(2!=1)
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
